df43d566f49694f7af8a001e4eb762853db9df51
Agustinus Juan Kurniawan <juankurniawan04@gmail.com>
2024-08-21 21:39:40 +0700

    OpenMusic V2

---
+++ src/utils/albums/index.js
@@ -0,0 +1,15 @@
+const mapAlbumsToModel = ({
+    id,
+    name,
+    year,
+    created_at,
+    updated_at,
+}) => ({
+    id,
+    name,
+    year,
+    createdAt: created_at,
+    updatedAt: updated_at,
+});
+
+module.exports = { mapAlbumsToModel };
